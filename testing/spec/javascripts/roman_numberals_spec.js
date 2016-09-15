fb98cd0817d3c402605f6a8b2c2f00653fa9ad8d
Thabo Titus <hello@thabotitus.co.za>
2016-09-15 14:16:57 +0200

    update config

--- testing/spec/javascripts/roman_numberals_spec.js
+++ testing/spec/javascripts/roman_numberals_spec.js
@@ -1,9 +1,9 @@
 describe("roman numberals kata", function() {
 
-  // it("1 to return I", function() {
-  //   convertor = new RomanNumerals();
-  //   expect(convertor.convert(1)).toBe('I');
-  // });
+  it("1 to return I", function() {
+    convertor = new RomanNumerals();
+    expect(convertor.convert(1)).toBe('I');
+  });
 
   // it("2 to return II", function() {
   //   convertor = new RomanNumerals();
