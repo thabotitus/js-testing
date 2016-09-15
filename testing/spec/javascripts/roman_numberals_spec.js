09cf36d719aee444862964d0ab7f6efcc217b0d5
Thabo Titus <hello@thabotitus.co.za>
2016-09-15 10:32:17 +0200

    testing config

--- testing/spec/javascripts/roman_numberals_spec.js
+++ testing/spec/javascripts/roman_numberals_spec.js
@@ -1,12 +1,12 @@
 describe("roman numberals kata", function() {
 
-  it("1 to return I", function() {
-    convertor = new RomanNumerals();
-    expect(convertor.convert(1)).toBe('I');
-  });
+  // it("1 to return I", function() {
+  //   convertor = new RomanNumerals();
+  //   expect(convertor.convert(1)).toBe('I');
+  // });
 
-/*  it("2 to return II", function() {*/
-    //convertor = new RomanNumerals();
-    //expect(convertor.convert(2)).toBe('II');
-  /*});*/
+  // it("2 to return II", function() {
+  //   convertor = new RomanNumerals();
+  //   expect(convertor.convert(2)).toBe('II');
+  // });
 });
